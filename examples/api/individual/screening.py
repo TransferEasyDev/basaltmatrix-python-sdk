3ef621602bc6c876fc771ee4162a6d0e72297cac
kongfm <kongp3@outlook>
2019-02-15 17:13:54 +0800

    跟随文档修改参数

--- examples/api/individual/screening.py
+++ examples/api/individual/screening.py
@@ -5,7 +5,6 @@ if __name__ == '__main__':
 
     params = {
         'name': '本',                    # 姓名	是
-        # 'country_code': 'CHN_ID',        # country_code	国家编码	否
         # 'dob': '1980/02/05',             # 出生日期	否	格式必须为 yyyy/mm/dd(1980/02/05)形式
         'gender': 'Male',               # 性别	否	Male/Female
         # 'max_count': '15',              # 最大匹配数量	否	默认为20，即按分值排序取前20条
